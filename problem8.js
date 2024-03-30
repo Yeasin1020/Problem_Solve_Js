cbed4bb0fc0295abfd560267c99673d88e4cd545
Yeasin1st <ysbd111@gmail.com>
2024-03-30 10:33:00 +0600

    random number match

---
+++ problem8.js
@@ -0,0 +1,9 @@
+let random = Math.ceil(Math.random()*10);
+console.log(random);
+let userNumber = 8;
+
+if (random === userNumber){
+	console.log("Good Work");
+}else{
+	console.log("Not matched");
+}
